d7a2e0da0b5b34f5421900773f42ab4363ca6bd8
Eduarte783 <eric.duarte83@gmail.com>
2022-07-16 01:06:06 -0700

    registration form is working

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
    const [values, setValues] = useState({
@@ -11,17 +13,41 @@ function Register() {
       confirmPassword: "",
    });
 
-   const handleSubmit = (event)=> {
-      event.preventDefault();
-      alert("form");
+   const toastOptions= {
+         position: "bottom-right",
+         pauseOnHover: 8000,
+         draggable: true,
+         theme: "dark",
    };
 
-   const handleValidation =() => {
-      const { password, confirmPassword, username, email} = values;
-      if (password !== confirmPasword) {       
-      }
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      handleValidation();
    };
-   
+
+   const handleValidation = () => {
+      const { password, confirmPassword, username, email } = values;
+      if (password !== confirmPassword) {
+         toast.error(
+            "Password and confirm password should be the same.", 
+            toastOptions);
+            return false;     
+         } else if (username.length < 3) {
+           toast.error("Username should be greater than 3 characters", toastOptions);
+           return false;
+         } else if (password.length < 6) {
+         toast.error(
+            "Password should be equal or greater than 6 characters",
+            toastOptions); 
+            return false;           
+         } else if(email === "") {
+         toast.error(
+            "email is required", toastOptions);
+            return false;
+         }
+         return true;
+      };
+
    const handleChange = (event) => {
       setValues({ ...values, [event.target.name]: event.target.value });
    };
@@ -49,13 +75,13 @@ function Register() {
                <input
                type="password"
                placeholder="Password"
-               name="passowrd"
+               name="password"
                onChange={(e) => handleChange(e)} 
                />
                <input
                type="password"
                placeholder="Confirm Password"
-               name="confirmPassowrd"
+               name="confirmPassword"
                onChange={(e) => handleChange(e)}   
                />
                <button type="submit">Create User</button>
@@ -63,6 +89,7 @@ function Register() {
                </span>
             </form>
          </FormContainer>
+         <ToastContainer />
     </>
     );
 }
